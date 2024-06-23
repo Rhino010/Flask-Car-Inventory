96f1f98ca76e9421bc0e823455443c0a4a313969
Ryan Boll <bollr38@hotmail.com>
2024-06-22 21:42:49 -0500

    gunicorn issue

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask
 from config import Config
 from .api.routes import api
 from .site.routes import site
 from .authentication.routes import auth
 
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 
-
-
-
 from json import JSONEncoder
-from models import db as root_db, login_manager, ma, User
+from models import db as root_db, login_manager, ma
 
 
 app = Flask(__name__)
@@ -22,8 +18,8 @@ app.register_blueprint(site)
 app.register_blueprint(auth)
 app.register_blueprint(api)
 
-app.config.from_object(Config)
 app.json_encoder = JSONEncoder
+app.config.from_object(Config)
 root_db.init_app(app)
 login_manager.init_app(app)
 ma.init_app(app)
